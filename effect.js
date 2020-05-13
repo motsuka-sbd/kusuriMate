5ad061278c60f662e93bf1f4764c157e14362005
motsukasbd <motsuka@sbdautomotive.com>
2020-05-13 19:45:24 +0900

    js追加

---
+++ effect.js
@@ -0,0 +1,7 @@
+// jQueryでフェードイン: https://www.sejuku.net/blog/57618
+$('#eduImg').fadeIn();
+
+// jQueryスクロールで要素をしたからFadeinさせる方法: https://qiita.com/art_porokyu/items/0c38f0ebb44fbdbb2893 
+$(window).scroll(function (){
+    
+})
